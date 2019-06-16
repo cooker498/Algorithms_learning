489c76a4b7d569a9bd61c4be4066c067d6775f8a
namedjw <namedjw@yeah.net>
2019-06-17 00:53:04 +0800

    add quick

---
+++ src/io/github/smdt/help/CompareHelper.java
@@ -0,0 +1,11 @@
+package io.github.smdt.help;
+
+/**
+ * @author daijw@inspur.com
+ * @version 1.0.0
+ * @ClassName CompareHelper.java
+ * @Description TODO
+ * @createTime 2019年06月17日 00:51:00
+ */
+public class CompareHelper {
+}
